149aaf25da9ceab613d17e296be2f37fabf8fac6
James Perreault <jperreault@classlink.com>
2022-08-23 23:32:00 -0400

    check http version too

--- routers/viewheaders.js
+++ routers/viewheaders.js
@@ -5,7 +5,12 @@ router.get("/", (req, res) => {
     // console.log(JSON.stringify(req.headers));
     // res.send('hello headers!');
     // res.send(`${Object.keys(req)}`);
-    res.send(JSON.stringify(req.headers));
+    // console.log(req.httpVersion);
+    // console.log(res.httpVersion);
+    const message = {};
+    message.headers = req.headers;
+    message.httpVersion = req.httpVersion;
+    res.send(JSON.stringify(message));
 });
 
 module.exports = router;
