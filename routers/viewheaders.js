8965fc9febe0b86c73af2b954229ed5243a7f0f0
James Perreault <jperreault@classlink.com>
2022-08-23 22:41:44 -0400

    nothing changed

--- routers/viewheaders.js
+++ routers/viewheaders.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = new express.Router();
-require("dotenv").config();
 
 router.get("/", (req, res) => {
     // console.log(JSON.stringify(req.headers));
