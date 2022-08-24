aa0ac3d68d891365d98603c17054d5023cb60a4b
James Perreault <jperreault@classlink.com>
2022-08-23 23:09:51 -0400

    enable sending headers

--- routers/viewheaders.js
+++ routers/viewheaders.js
@@ -3,9 +3,9 @@ const router = new express.Router();
 
 router.get("/", (req, res) => {
     // console.log(JSON.stringify(req.headers));
-    res.send('hello headers!');
+    // res.send('hello headers!');
     // res.send(`${Object.keys(req)}`);
-    // res.send(JSON.stringify(req.headers));
+    res.send(JSON.stringify(req.headers));
 });
 
 module.exports = router;
